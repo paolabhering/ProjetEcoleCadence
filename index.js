b70afe6fc89d354ce8356a21ce2f22d09c19ae03
MartineDesmarais <166078856+MartineDesmarais@users.noreply.github.com>
2024-10-05 19:29:42 -0400

    formulaire ajout de costume

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const hbs = require('express-handlebars');
 const db = require("./db");
 const app = express();
-
 const path = require('path');
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
-app.get('/', function (req, res) {
-res.render('catalogue')
-})
+// pour lier les routers
+const routerAjoutCostume = require("./routerAjoutCostume");
+
+// pour utiliser les routers
+app.use("/", routerAjoutCostume);
+
+// pour envoyer la liste des produits à la page catalogue.handlebars
+// app.get("/", async function(req,res) {
+//     const {rows} = await db.execute ("SELECT * FROM costumes")
+//    res.render("catalogue", {listeCostumes: rows});
+//    console.log(rows);
+// })
 
 
-app.listen(3000, function() {
-console.log(`Serveur sur le port 3000`)
-})
+
+// router.get("/ajout", function (req, res) {    
+//     res.render("ajoutCostume");
+// });
+
+//  app.get('/', function (req, res) {
+//  res.render('ajoutCostume');
+//  });
+
+
+app.listen(3001, function() {
+console.log(`Serveur sur le port 3001`);
+});
---
+++ routerAjoutCostume.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const multer = require('multer'); // pour télécharger des fichiers
+const {createClient} = require ("@libsql/client");
+
+const router = express.Router();
+
+// const exphbs = require("express-handlebars");
+
+
+const storage =  multer.diskStorage({   
+    destination : (req, file, cb) => {
+        cb(null, './static/img/')           //télcharge le fichier en l'envoyant dans la destination (fichier) précisée
+    },
+    filename : (req, file, cb) => {
+        cb(null, file.originalname);        //fonction callback qui le fichier avec le nom du fichier
+    }                                   
+})
+
+const upload = multer({storage : storage});
+
+router.use(express.urlencoded({extended: true}));
+
+const db = createClient({
+    url: "file:catalogue.db"
+})
+ 
+
+
+router.get("/ajout", function (req, res) {    
+    res.render("ajoutCostume");
+});
+
+router.post("/ajout", upload.single('upload_photo'), function(req,res) {
+    let couleurs = [];
+        if (req.body.brun) couleurs.push("brun");
+        if (req.body.rouge) couleurs.push("rouge");
+        if (req.body.rose) couleurs.push("rose");
+        if (req.body.mauve) couleurs.push("mauve");
+        if (req.body.bleu) couleurs.push("bleu");
+        if (req.body.vert) couleurs.push("vert");
+        if (req.body.jaune) couleurs.push("jaune");
+        if (req.body.orange) couleurs.push("orange");
+        if (req.body.noir) couleurs.push("noir");
+        if (req.body.gris) couleurs.push("gris");
+        if (req.body.blanc) couleurs.push("blanc");
+        if (req.body.multi) couleurs.push("multi");
+    console.log({
+        id: req.body.id,
+        titre: req.body.titre, 
+        categorie: req.body.categorie,
+        age_group: req.body.age_group,
+        couleurs: couleurs.join(", "),
+        note: req.body.note,
+        localisation: req.body.localisation,
+        boite: req.body.boite,
+        image: "/img" + req.file.filename,
+    })
+    db.execute({
+        sql: "INSERT INTO costumes(titre, categorie, age_group, couleur, note, localisation, boite, image) VALUES(:titre, :categorie, :age_group, :couleur, :note, :localisation, :boite,:image)",
+        args:{
+            titre: req.body.titre, 
+            categorie: req.body.categorie,
+            age_group: req.body.age_group,
+            couleurs: couleurs.join(", "),
+            note: req.body.note,
+            localisation: req.body.localisation,
+            boite: req.body.boite,
+            image: "/img" + req.file.filename,
+        },
+    })
+    res.redirect("/");
+})
+
+module.exports = router;
