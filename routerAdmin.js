f41a76fab8452a17f278104abc334278ad30640d
paolabhering <156713438+paolabhering@users.noreply.github.com>
2024-11-10 15:27:52 -0500

    Update routerAdmin.js

--- routerAdmin.js
+++ routerAdmin.js
@@ -8,8 +8,29 @@ const routerLogOut = require('./routerLogOut');
 const exphbs = require("express-handlebars");
 router.use(express.urlencoded({ extended: true }));
 
-router.get("/admin", ensureAuthenticated, restrictToRole("administrateur"), (req, res) => {
-    res.render("admin");
+router.get("/admin", ensureAuthenticated, restrictToRole("administrateur"), async (req, res) => {
+    try {
+        const userId = req.session.user.user_id;
+        console.log("User id stored in session is", userId); 
+        const query = await db.execute(
+            `SELECT langue FROM users WHERE user_id = ?`,
+            [userId] 
+        );
+
+        const result = query.rows[0];
+        const userLangue = result.langue;
+        console.log("User's chosen language is:", userLangue);
+
+        if (userLangue === 'fr') {
+            
+            res.render("admin", { userLangue });
+        } else {
+            res.render("adminEN", { userLangue });
+        }
+    } catch (error) {
+        console.error("Error fetching language preference:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 router.use('/', routerLogOut);
