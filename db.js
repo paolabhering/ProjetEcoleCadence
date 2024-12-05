f424e2c78599d506d8d1155977b79ef6d92aab36
MartineDesmarais <166078856+MartineDesmarais@users.noreply.github.com>
2024-12-04 19:32:28 -0500

    mÀj

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 
-const {createClient} = require("@libsql/client")
+const {createClient} = require("@libsql/client");
+const path = require("path");
 
 module.exports = createClient({
     url: `file:${path.join(__dirname, 'var/www/projetecolecadence/ProjetEcoleCadence/catalogue.db')}`
